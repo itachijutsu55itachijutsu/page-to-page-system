807151c5150661dfc7b8aa68beac077d8587effb
Full-Stack Master <itachijutsu96@gmail.com>
2026-09-26 13:25:41 +0500

    Update page.js

--- app/page2/page.js
+++ app/page2/page.js
@@ -1,12 +1,19 @@
 'use client';
 
 import { useEffect } from 'react';
+import Script from 'next/script';
 
 const TARGET_URL = 'https://www.profitableratecpmnetwork.com/pr05z0d17?key=8c2d662b33c1776277adf8cc2c0433f0'; // <-- apna URL yahan replace karein
+const GA_MEASUREMENT_ID = 'G-KPFJDCXDLR'; // <-- apna GA4 Measurement ID yahan daalein
 
 export default function Page2() {
   useEffect(() => {
-    window.location.replace(TARGET_URL);
+    // Thoda delay taake GA event fire hone ka time mil jaye redirect se pehle
+    const timer = setTimeout(() => {
+      window.location.replace(TARGET_URL);
+    }, 600);
+
+    return () => clearTimeout(timer);
   }, []);
 
   return (
@@ -20,6 +27,22 @@ export default function Page2() {
         background: '#000',
       }}
     >
+      {/* Google Analytics (GA4) */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
       <img
         src="/page2.jpg"
         alt=""
